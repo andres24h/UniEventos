cecd55560abe55e6ab7d4f6d6e5efcfc42ca94b9
Luisc00 <luisc.moralesc@uqvirtual.edu.co>
2024-09-09 15:40:23 -0500

    Servicio imagenes implementado y tokens en el anterior commit

--- src/main/java/co/edu/uniquindio/unieventos/servicios/impl/ImagenesServicioImpl.java
+++ src/main/java/co/edu/uniquindio/unieventos/servicios/impl/ImagenesServicioImpl.java
@@ -1,4 +1,40 @@
 package co.edu.uniquindio.unieventos.servicios.impl;
 
-public class ImagenesServicioImpl {
+
+import co.edu.uniquindio.unieventos.servicios.interfaces.ImagenesServicio;
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+@Service
+public class ImagenesServicioImpl implements ImagenesServicio {
+    private final Cloudinary cloudinary;
+    public ImagenesServicioImpl() {
+        Map<String, String> config = new HashMap<>();
+        config.put("cloud_name", "dqkoltbf0");
+        config.put("api_key", "818749439566557");
+        config.put("api_secret", "TNNvJ8q4fXP9WWGX0bN0J_gnvvA");
+        cloudinary = new Cloudinary(config);
+    }
+    @Override
+    public Map subirImagen(MultipartFile imagen) throws Exception {
+        File file = convertir(imagen);
+        return cloudinary.uploader().upload(file, ObjectUtils.asMap("folder", "unilocal"));
+    }
+    @Override
+    public Map eliminarImagen(String idImagen) throws Exception {
+        return cloudinary.uploader().destroy(idImagen, ObjectUtils.emptyMap());
+    }
+    private File convertir(MultipartFile imagen) throws IOException {
+        File file = File.createTempFile(imagen.getOriginalFilename(), null);
+        FileOutputStream fos = new FileOutputStream(file);
+        fos.write(imagen.getBytes());
+        fos.close();
+        return file;
+    }
 }
