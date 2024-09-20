a05b69ffc39323cd9f5212aa69f09ad680dd6034
Luisc00 <luisc.moralesc@uqvirtual.edu.co>
2024-09-20 16:10:38 -0500

    Update OrdenServicioImpl.java

--- src/main/java/co/edu/uniquindio/unieventos/servicios/impl/OrdenServicioImpl.java
+++ src/main/java/co/edu/uniquindio/unieventos/servicios/impl/OrdenServicioImpl.java
@@ -1,4 +1,10 @@
 package co.edu.uniquindio.unieventos.servicios.impl;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
 public class OrdenServicioImpl {
+
 }
