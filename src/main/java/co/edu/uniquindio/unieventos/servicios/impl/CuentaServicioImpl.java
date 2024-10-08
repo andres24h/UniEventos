ffb9ac3332a0d05fe6095b2037a752f49eac26e4
Luisc00 <luisc.moralesc@uqvirtual.edu.co>
2024-10-08 17:43:41 -0500

    .

--- src/main/java/co/edu/uniquindio/unieventos/servicios/impl/CuentaServicioImpl.java
+++ src/main/java/co/edu/uniquindio/unieventos/servicios/impl/CuentaServicioImpl.java
@@ -85,7 +85,13 @@ public class CuentaServicioImpl implements CuentaServicio {
         Duration duracionValidez = Duration.ofMinutes(15);
 
         if (Duration.between(fechaCreacion, fechaActual).compareTo(duracionValidez) > 0) {
+            cuenta.setCodigoValidacionRegistro(new CodigoValidacion(generarCodigo(), LocalDateTime.now()));
+            EmailDTO emailDTO=new EmailDTO("Su codigo de activacion es:"+cuenta.getCodigoValidacionRegistro(),"ingrese el codigo para poder activar su cuenta",cuenta.email());
+
+            emailServicio.enviarCorreo(emailDTO);
+            cuentaRepo.save(cuenta);
             throw new Exception("El código de verificación ha expirado");
+            
         }
 
         cuenta.setEstado(EstadoCuenta.ACTIVO);
