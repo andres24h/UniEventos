5d5eba586d10024d7084bdbc10143fa2a647cd99
CamiloAst <juanc.astudilloo@uqvirtual.edu.co>
2024-10-10 21:54:12 -0500

    EndPoints para los Filtros de eventos

--- src/main/java/co/edu/uniquindio/unieventos/controladores/EventoControlador.java
+++ src/main/java/co/edu/uniquindio/unieventos/controladores/EventoControlador.java
@@ -1,5 +1,6 @@
 package co.edu.uniquindio.unieventos.controladores;
 
+import co.edu.uniquindio.unieventos.documentos.TipoEvento;
 import co.edu.uniquindio.unieventos.dto.evento.*;
 import co.edu.uniquindio.unieventos.dto.global.MensajeDTO;
 import co.edu.uniquindio.unieventos.servicios.interfaces.EventoServicio;
@@ -69,5 +70,59 @@ public class EventoControlador {
         List<ItemEventoDTO> lista = eventoServicio.filtrarEventos(filtroEventoDTO);
         return ResponseEntity.ok(new MensajeDTO<>(false, lista));
     }
+    @GetMapping("/filtrar-por-ciudad/{ciudad}")
+    public ResponseEntity<MensajeDTO<List<ItemEventoDTO>>> filtrarEventosPorCiudad(@PathVariable String ciudad) {
+        List<ItemEventoDTO> lista = eventoServicio.filtrarEventosPorCiudad(ciudad);
+        return ResponseEntity.ok(new MensajeDTO<>(false, lista));
+    }
 
+    @GetMapping("/filtrar-por-tipo/{tipo}")
+    public ResponseEntity<MensajeDTO<List<ItemEventoDTO>>> filtrarEventosPorTipo(@PathVariable TipoEvento tipo) {
+        List<ItemEventoDTO> lista = eventoServicio.filtrarEventosPorTipo(tipo);
+        return ResponseEntity.ok(new MensajeDTO<>(false, lista));
+    }
+
+    @GetMapping("/filtrar-por-nombre/{nombre}")
+    public ResponseEntity<MensajeDTO<List<ItemEventoDTO>>> filtrarEventosPorNombre(@PathVariable String nombre) {
+        List<ItemEventoDTO> lista = eventoServicio.filtrarEventosPorNombre(nombre);
+        return ResponseEntity.ok(new MensajeDTO<>(false, lista));
+    }
+
+    @PostMapping("/filtrar-por-nombre-y-ciudad")
+    public ResponseEntity<MensajeDTO<List<ItemEventoDTO>>> filtrarEventosPorNombreYCiudad(@Valid @RequestBody FiltrarPorNombreYCiudadDTO filtro) {
+        List<ItemEventoDTO> lista = eventoServicio.filtrarEventosPorNombreYCiudad(filtro);
+        return ResponseEntity.ok(new MensajeDTO<>(false, lista));
+    }
+
+    @PostMapping("/filtrar-por-fecha")
+    public ResponseEntity<MensajeDTO<List<ItemEventoDTO>>> filtrarEventosPorFecha(@Valid @RequestBody FiltrarPorFechaDTO filtro) {
+        List<ItemEventoDTO> lista = eventoServicio.filtrarEventosPorFecha(filtro);
+        return ResponseEntity.ok(new MensajeDTO<>(false, lista));
+    }
+
+    @PostMapping("/filtrar-por-fecha-y-ciudad")
+    public ResponseEntity<MensajeDTO<List<ItemEventoDTO>>> filtrarEventosPorFechaYCiudad(@Valid @RequestBody FiltrarPorFechaYCiudadDTO filtro) {
+        List<ItemEventoDTO> lista = eventoServicio.filtrarEventosPorFechaYCiudad(filtro);
+        return ResponseEntity.ok(new MensajeDTO<>(false, lista));
+    }
+
+    @PostMapping("/filtrar-por-fecha-y-tipo")
+    public ResponseEntity<MensajeDTO<List<ItemEventoDTO>>> filtrarEventosPorFechaYTipo(@Valid @RequestBody FiltrarPorFechaYTipoDTO filtro) {
+        List<ItemEventoDTO> lista = eventoServicio.filtrarEventosPorFechaYTipo(filtro);
+        return ResponseEntity.ok(new MensajeDTO<>(false, lista));
+    }
+
+    @PostMapping("/filtrar-por-fecha-tipo-y-ciudad")
+    public ResponseEntity<MensajeDTO<List<ItemEventoDTO>>> filtrarEventosPorFechaYTipoYCiudad(@Valid @RequestBody FiltrarPorFechaYTipoYCiudadDTO filtro) {
+        List<ItemEventoDTO> lista = eventoServicio.filtrarEventosPorFechaYTipoYCiudad(filtro);
+        return ResponseEntity.ok(new MensajeDTO<>(false, lista));
+    }
+
+    @PostMapping("/filtrar-por-tipo-y-ciudad")
+    public ResponseEntity<MensajeDTO<List<ItemEventoDTO>>> filtrarEventosPorTipoYCiudad(@Valid @RequestBody FiltrarPorTipoYCiudadDTO filtro) {
+        List<ItemEventoDTO> lista = eventoServicio.filtrarEventosPorTipoYCiudad(filtro);
+        return ResponseEntity.ok(new MensajeDTO<>(false, lista));
+    }
 }
+
+
